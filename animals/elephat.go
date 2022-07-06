14ec6b0473f3b5df2f31b375a0b68957123be475
GenkiHirano <genki86web@gmail.com>
2022-07-06 15:12:44 +0900

    feat: zooアプリケーション作成

---
+++ animals/elephat.go
@@ -0,0 +1,5 @@
+package animals
+
+func ElephatFeed() string {
+	return "Grass"
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"zoo/animals"
+)
+
+func main() {
+	fmt.Println(animals.ElephatFeed())
+	fmt.Println(animals.MonkeyFeed())
+	fmt.Println(animals.RabbitFeed())
+}
